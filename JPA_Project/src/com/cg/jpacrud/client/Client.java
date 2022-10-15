8e78ce33e08cc76d267e750bef69f3540b434455
Arrow technologies <Arrow technologies@DESKTOP-P254STU>
2022-10-15 13:46:19 +0530

    commit

--- JPA_Project/src/com/cg/jpacrud/client/Client.java
+++ JPA_Project/src/com/cg/jpacrud/client/Client.java
@@ -14,7 +14,7 @@ public class Client {
 		System.out.println("Total books:"+service.getBookCount());
 		
 		System.out.println("************Listing book with id 105*************");
-		System.out.println("Book with ID 106:"+service.getBookById(105));
+		System.out.println("Book with ID 105:"+service.getBookById(105));
 		
 		System.out.println("************Listing All books*************");
 		for(Book book:service.getAllBooks()) {
@@ -30,8 +30,6 @@ public class Client {
 		for(Book book:service.getBookByTitle("Android")) {
 			System.out.println(book);
 		}
-		
-		
 		
 		System.out.println("************Listing All books between 500 and 600*************");
 		for(Book book:service.getBooksInPriceRange(500, 600) ) {
--- JPA_Project/src/com/cg/jpacrud/service/BookService.java
+++ JPA_Project/src/com/cg/jpacrud/service/BookService.java
@@ -1,4 +1,5 @@
 package com.cg.jpacrud.service;
+
 import java.util.List;
 
 import com.cg.jpacrud.entities.Book;
